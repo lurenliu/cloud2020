f3df2ff04ac6e730a26f5b9a397d030c0872d502
我有一瓢酒 <1203603228@qq.com>
2020-12-14 17:26:09 +0800

    添加消息总线 中 消息的消费者（consumer） 8802

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/controller/ReviceMessageController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tiam
+ * @version 1.0
+ * @date 2020/12/14 17:07
+ */
+@Component
+@EnableBinding(Sink.class)
+public class ReviceMessageController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        System.out.println("~~~~~ 消费者1号， 获取到的消息Message : " + message.getPayload() + "\t" + " serverPort : " + serverPort);
+    }
+
+}
